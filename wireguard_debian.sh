82b7e3d3420d20d53b035c236141332d4e296f3d
DivineFarm <139358991+DivineFarm@users.noreply.github.com>
2023-12-25 19:53:17 +0100

    Update wireguard_debian.sh

--- wireguard_debian.sh
+++ wireguard_debian.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
 # Determine the Internet facing network interface
-# You can use a command like the following to determine the internet-facing interface
-# interface=$(command to determine the internet-facing interface)
-
-# Change ens6 on all the relevant lines with the interface name
-# Replace "ens6" with the actual interface name in the following commands
+MAIN_INTERFACE=$(ip route | grep default | awk '{print $5}')
 
 # Install Docker
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg -y
+sudo apt-get install ca-certificates curl gnupg net-tools -y
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 sudo systemctl enable docker
@@ -29,23 +25,33 @@ sudo ufw allow 22/tcp
 sudo ufw allow 5000/tcp
 sudo ufw allow 9443/tcp
 sudo ufw allow 51820/udp
+sudo ufw route allow in on wg0 out on $MAIN_INTERFACE; iptables -t nat -I POSTROUTING -o $MAIN_INTERFACE -j MASQUERADE
 sudo ufw reload
 sudo ufw enable
 
 # Get the network interface name
-NETWORK_INTERFACE=$(ifconfig | grep -oP 'inet\b{1,2}')
-
-# Get the IP address of the network interface
-IP_ADDRESS=$(ifconfig | grep -oP "inet\b{1,2} \(${IP_ADDR})")
+IP_ADDRESS=$(ip addr show $MAIN_INTERFACE | grep -oP 'inet \K[\d.]+')
 
 # Replace SERVER-IP with the machine's IP
 MACHINE_IP=$(echo $IP_ADDRESS | cut -d/ -f 1)
 
 # Echo the lines with the replaced IP
+echo "----------------------------------------------------------------------------------------------------------------------"
 echo "web-ui: http://$MACHINE_IP:5000   (admin/admin)"
 echo "Portainer: https://$MACHINE_IP:9443 (admin/password change request at first login)"
+echo "----------------------------------------------------------------------------------------------------------------------"
+echo "Set the Wireguard PostUp and PostDown options to:"
+echo "FULL TUNNEL CONFIGURATION:"
+echo "PostUp = iptables -w -t nat -A POSTROUTING -o $MAIN_INTERFACE -j MASQUERADE; ip6tables -w -t nat -A POSTROUTING -o" $MAIN_INTERFACE " -j MASQUERADE"
+echo "PostDown = iptables -w -t nat -D POSTROUTING -o ens6 -j MASQUERADE; ip6tables -w -t nat -D POSTROUTING -o" $MAIN_INTERFACE " -j MASQUERADE"
+echo "    "
+echo "SPLIT TUNNEL CONFIGURATION:"
+echo "PostUp = iptables -t mangle -A PREROUTING -i wg0 -j MARK --set-mark 0x30; iptables -t nat -A POSTROUTING ! -o wg0 -m mark --mark 0x30 -j MASQUERADE"
+echo "PostDown = iptables -t mangle -D PREROUTING -i wg0 -j MARK --set-mark 0x30; iptables -t nat -D POSTROUTING ! -o wg0 -m mark --mark 0x30 -j MASQUERADE"
 
-# Additional instructions
+echo "---------------------------------------------------------------------------------------------------------------------"
+echo "Additional instructions :"
+
 echo "To change the password to the Wireguard UI:"
 echo "1- login to portainer"
 echo "2- Go to containers"
